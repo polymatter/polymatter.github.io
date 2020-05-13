01e45fcc28a51c6a62ee37d1ce39b996f166ef68
polymatter <jlake.jobs@gmail.com>
2020-05-13 16:35:03 +0100

    test extracting element value

--- weather/weather.js
+++ weather/weather.js
@@ -1,7 +1,7 @@
 import * as ELEMENTS from './uiElements.js'
 
 let searchWeather = () => {
-    console.log(ELEMENTS.PROMPT_CITY.textContent);
+    console.log(ELEMENTS.PROMPT_CITY.value);
 }
 
 ELEMENTS.PROMPT_BUTTON.addEventListener('click', searchWeather)
