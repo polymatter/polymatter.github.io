00b307c63762fef18b56bdfdd62082ed6aff369c
polymatter <jlake.jobs@gmail.com>
2020-05-13 18:39:42 +0100

    test if process.env contains github secret

--- weather/weather.js
+++ weather/weather.js
@@ -1,4 +1,6 @@
 import * as ELEMENTS from './uiElements.js';
 import { searchWeather } from './fetch.js';
 
+console.log('TEST SECRET', process.env.TEST_SECRET)
+
 ELEMENTS.PROMPT_BUTTON.addEventListener('click', () => searchWeather(ELEMENTS.PROMPT_CITY.value))
