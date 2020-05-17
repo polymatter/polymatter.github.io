33ca9ab830d3df64528f8d81986797dfd27bea12
polymatter <jlake.jobs@gmail.com>
2020-05-17 18:41:56 +0100

    attempt to get jekyll deploying ok

--- weather/weather.js
+++ weather/weather.js
@@ -1,6 +1,6 @@
 import * as ELEMENTS from './uiElements.js';
 import { searchWeather } from './fetch.js';
 
-console.log('TEST SECRET', secrets?.TEST_SECRET)
+// console.log('TEST SECRET', secrets?.TEST_SECRET)
 
 ELEMENTS.PROMPT_BUTTON.addEventListener('click', () => searchWeather(ELEMENTS.PROMPT_CITY.value))
