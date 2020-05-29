2455b604c630a584cdca19ae7648dd75d69d13b3
polymatter <jlake.jobs@gmail.com>
2020-05-29 18:30:14 +0100

    display results

--- weather/masterController.js
+++ weather/masterController.js
@@ -5,6 +5,7 @@ ELEMENTS.PROMPT_BUTTON.addEventListener('click', () => {
     const CITY = ELEMENTS.PROMPT_CITY.value;
     searchWeather(CITY).then(data => {
         console.log(`received data ${data}`);
+        ELEMENTS.RESULT.style.display = 'block';
         ELEMENTS.RESULT_DESCRIPTION.innerHTML = data.weather[0].description;
     })
 });
