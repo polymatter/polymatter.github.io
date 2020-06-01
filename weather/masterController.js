1a75b9f07f5eaa3962611b6c6dd5ccb6453d9339
polymatter <jlake.jobs@gmail.com>
2020-06-01 17:29:35 +0100

    add flex

--- weather/masterController.js
+++ weather/masterController.js
@@ -5,7 +5,7 @@ ELEMENTS.PROMPT_BUTTON.addEventListener('click', () => {
     const CITY = ELEMENTS.PROMPT_CITY.value;
     ELEMENTS.RESULT.hide();
     searchWeather(CITY).then(data => {
-        ELEMENTS.RESULT.show();
+        ELEMENTS.RESULT.show('flex');
         ELEMENTS.RESULT.setDescription(data.description);
         ELEMENTS.RESULT.setTemperature(data.temperature);
         ELEMENTS.RESULT.setIconURL(data.iconURL);
