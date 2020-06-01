71edb38a24ba79d8e78eda3a85173a17d9edf581
polymatter <jlake.jobs@gmail.com>
2020-06-01 14:11:36 +0100

    mediate between weather api response and controller

--- weather/masterController.js
+++ weather/masterController.js
@@ -6,9 +6,7 @@ ELEMENTS.PROMPT_BUTTON.addEventListener('click', () => {
     ELEMENTS.RESULT.hide();
     searchWeather(CITY).then(data => {
         ELEMENTS.RESULT.show();
-        const DESCRIPTION = data.weather[0].description;
-        const TEMPERATURE = data.main.temp;
-        ELEMENTS.RESULT.setDescription(DESCRIPTION);
-        ELEMENTS.RESULT.setTemperature(TEMPERATURE);
+        ELEMENTS.RESULT.setDescription(data.description);
+        ELEMENTS.RESULT.setTemperature(data.temperature);
     })
 });
--- weather/weatherAPI.js
+++ weather/weatherAPI.js
@@ -2,6 +2,20 @@ import { WEATHER_API_KEY } from '../config.js'
 
 const URL_PATTERN = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}&units=metric';
 
+class WeatherResponse {
+    constructor(data) {
+        this.data = data;
+    }
+
+    get description() {
+        return data.weather[0].description;
+    }
+
+    get temperature() {
+        return data.main.temp;
+    }
+}
+
 export const searchWeather = (city) => new Promise((resolve, reject) => {
     const URL = URL_PATTERN.replace('{city name}', city).replace('{your api key}', WEATHER_API_KEY);
     console.log(`requesting from url ${URL}`)
@@ -11,7 +25,7 @@ export const searchWeather = (city) => new Promise((resolve, reject) => {
         if (REQUEST.readyState === XMLHttpRequest.DONE && REQUEST.status == "200") {
             console.log(`received data ${REQUEST.responseText}`)
             const RESPONSE = JSON.parse(REQUEST.responseText);
-            resolve(RESPONSE);
+            resolve(new WeatherResponse(RESPONSE));
         } else if (REQUEST.readyState === XMLHttpRequest.DONE) {
             console.error(REQUEST);
             reject(REQUEST.responseType);
