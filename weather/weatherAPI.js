f7eca23e6d87a94a85ffdb15a371f593cf57f029
polymatter <jlake.jobs@gmail.com>
2020-06-01 14:58:30 +0100

    cleanup logs

--- weather/weatherAPI.js
+++ weather/weatherAPI.js
@@ -1,6 +1,6 @@
 import { WEATHER_API_KEY } from '../config.js'
 
-const URL_PATTERN = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}&units=metric';
+const WEATHER_FOR_CITY_PATTERN = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}&units=metric';
 const ICON_PATTERN = 'https://openweathermap.org/img/wn/{icon code}@2x.png';
 
 class WeatherResponse {
@@ -22,13 +22,11 @@ class WeatherResponse {
 }
 
 export const searchWeather = (city) => new Promise((resolve, reject) => {
-    const URL = URL_PATTERN.replace('{city name}', city).replace('{your api key}', WEATHER_API_KEY);
-    console.log(`requesting from url ${URL}`)
+    const URL = WEATHER_FOR_CITY_PATTERN.replace('{city name}', city).replace('{your api key}', WEATHER_API_KEY);
     const REQUEST = new XMLHttpRequest();
     REQUEST.open('GET', URL);
     REQUEST.onreadystatechange = () => {
         if (REQUEST.readyState === XMLHttpRequest.DONE && REQUEST.status == "200") {
-            console.log(`received data ${REQUEST.responseText}`)
             const RESPONSE = JSON.parse(REQUEST.responseText);
             resolve(new WeatherResponse(RESPONSE));
         } else if (REQUEST.readyState === XMLHttpRequest.DONE) {
