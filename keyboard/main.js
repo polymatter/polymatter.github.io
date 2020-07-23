14d871e3274da5f1e72172ab523708cbb0269229
polymatter <jlake.jobs@gmail.com>
2020-07-23 18:16:56 +0100

    fix removing too many characters and add more vn keys

--- keyboard/main.js
+++ keyboard/main.js
@@ -4,10 +4,28 @@ const log = document.querySelector('#debug');
 input.oninput = replaceKey;
 
 const translations = {
+    '`': '`',
+    '¬': '~',
     '!': 'Ă',
+    '\"': 'Â',
+    '£': 'Ê',
+    '$': 'Ô',
+    '%': '%',
+    '^': '^',
+    '&': '&',
+    '*': '*',
+    '(': '(',
+    ')': 'Đ',
     '1': 'ă',
     '2': 'â',
-    '3': 'ê'
+    '3': 'ê',
+    '4': 'ô',
+    '5': '',
+    '6': '',
+    '7': '',
+    '8': '',
+    '9': '',
+    '0': 'đ'
 };
 
 function translate(keyPress) {
@@ -16,6 +34,6 @@ function translate(keyPress) {
 
 function replaceKey(e) {
     if (e.inputType === 'insertText') {
-        input.value = input.value.substring(0, input.value.length - 2) + translate(e.data);
+        input.value = input.value.substring(0, input.value.length - 1) + translate(e.data);
     }
 }
