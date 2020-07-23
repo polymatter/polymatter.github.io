26e74d2afff2b9d6da0fa9c8375110c12df553dd
polymatter <jlake.jobs@gmail.com>
2020-07-23 18:03:55 +0100

    replace in box and have quick translation matrix

--- keyboard/main.js
+++ keyboard/main.js
@@ -1,10 +1,21 @@
 const input = document.querySelector('.input');
 const log = document.querySelector('#debug');
 
-input.onkeydown = logKey;
+input.oninput = replaceKey;
 
-function logKey(e) {
-  log.textContent += ` ${e.code}`;
-  input.textContent = input.textContent.substring(0, input.textContent.length - 2) + e.code;
-  console.log(e);
+const translations = {
+    '!': 'Ă',
+    '1': 'ă',
+    '2': 'â',
+    '3': 'ê'
+};
+
+function translate(keyPress) {
+    return translations[keyPress] || keyPress;
+}
+
+function replaceKey(e) {
+    if (e.inputType === 'insertText') {
+        input.value = input.value.substring(0, input.value.length - 2) + translate(e.data);
+    }
 }
