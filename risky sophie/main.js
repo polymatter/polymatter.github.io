9a342f9940d171a7c2f7d65c3a67aaa622e6f3ec
polymatter <jlake.jobs@gmail.com>
2020-10-29 21:14:31 +0000

    initial load info from json call

---
+++ risky sophie/main.js
@@ -0,0 +1,44 @@
+
+document.body.onload = fetchUpdateData;
+
+function fetchUpdateData() {
+    fetch('https://jsonstorage.net/api/items/26a9423e-7389-4cfb-a26c-99236a8f7ba7')
+    .then(response => response.json())
+    .then(data => updateDisplay(data));
+}
+
+function updateDisplay(data) {
+    console.log(data);
+    let risks = data.risks;
+    let container = document.querySelector('#dashboard');
+
+    risks.forEach(risk => {
+        let riskui = document.createElement('article');
+        let label = document.createElement('h1');
+        let labelText = document.createTextNode(risk.label);
+        label.appendChild(labelText);
+
+        riskui.classList = ["dashboard-element"];
+        container.appendChild(riskui);
+    });
+}
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
