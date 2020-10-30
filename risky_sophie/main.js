0c808dbc1930d5cb7e77e44fdfac8d81a688b39c
polymatter <jlake.jobs@gmail.com>
2020-10-30 09:27:19 +0000

    formatting

--- risky_sophie/main.js
+++ risky_sophie/main.js
@@ -2,44 +2,43 @@
 document.body.onload = fetchUpdateData;
 
 function fetchUpdateData() {
-    fetch('https://jsonstorage.net/api/items/26a9423e-7389-4cfb-a26c-99236a8f7ba7')
+  fetch('https://jsonstorage.net/api/items/26a9423e-7389-4cfb-a26c-99236a8f7ba7')
     .then(response => response.json())
     .then(data => updateDisplay(data));
 }
 
 function updateDisplay(data) {
-    console.log(data);
-    let risks = data.risks;
-    let container = document.querySelector('#dashboard');
+  console.log(data);
+  let risks = data.risks;
+  let container = document.querySelector('#dashboard');
 
-    risks.forEach(risk => {
-        let riskui = document.createElement('article');
-        let label = document.createElement('h1');
-        let labelText = document.createTextNode(risk.label);
-        label.appendChild(labelText);
-        riskui.appendChild(label);
+  risks.forEach(risk => {
+    let riskui = document.createElement('article');
+    let label = document.createElement('h1');
+    let labelText = document.createTextNode(risk.label);
+    label.appendChild(labelText);
+    riskui.appendChild(label);
 
-        riskui.classList = ["dashboard-element"];
-        container.appendChild(riskui);
-    });
+    riskui.classList = ["dashboard-element"];
+    container.appendChild(riskui);
+  });
 }
 
 // Example POST method implementation:
 async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-  
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
